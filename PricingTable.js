f3f0b3ef6fe6cb9db967990c830125b28704d316
QuocAnh2001 <nguyenquocanh19122901@gmail.com>
2023-02-06 15:46:43 +0700

    sua ham makeSlideshow

--- PricingTable.js
+++ PricingTable.js
@@ -1,11 +1,9 @@
 let slideIndex = 0;
-let currentIndex = 0;
-currentIndex = slideIndex;
 let prevBtn = document.querySelector(".prev");
 let nextBtn = document.querySelector(".next");
 let slides = document.querySelectorAll(".col");
 let dot = document.querySelectorAll(".dot");
-const auto_time = 5000;
+const AUTO_TIME = 5000;
 
 nextBtn.addEventListener("click", () => {
   if (slideIndex >= slides.length - 1) {
@@ -13,7 +11,10 @@ nextBtn.addEventListener("click", () => {
   } else {
     slideIndex++;
   }
-  makeSlideshow(currentIndex);
+  makeSlideshow(slideIndex);
+  dot.forEach((dots) => {
+    dots.className = dots.className.replace("", "");
+  });
 });
 
 prevBtn.addEventListener("click", () => {
@@ -22,30 +23,26 @@ prevBtn.addEventListener("click", () => {
   } else {
     slideIndex--;
   }
-  makeSlideshow(currentIndex);
+  makeSlideshow(slideIndex);
 });
-
-function currentSlide(n) {
-  makeSlideshow((slideIndex = n));
-}
-function makeSlideshow() {
-  let i;
-  for (i = 0; i < slides.length; i++) {
-    slides[i].style.display = "none";
-  }
-  slides[slideIndex].style.display = "block";
-  for (i = 0; i < dot.length; i++) {
-    dot[i].className = dot[i].className.replace(" active", "");
-  }
-  dot[slideIndex].className += " active";
+function makeSlideshow(index) {
+  slides.forEach((item) => {
+    item.style.display = "none";
+  });
+  slides[index].style.display = "block";
+  dot.forEach((dots) => {
+    dots.className = dots.className.replace(" active", "");
+  });
+  slideIndex = index;
+  dot[index].className += " active";
 }
 function makeSlideshowauto() {
   // console.log(`${slideIndex} auto `);
-  makeSlideshow();
+  makeSlideshow(slideIndex);
   slideIndex++;
   if (slideIndex > slides.length - 1) {
     slideIndex = 0;
   }
 }
-setInterval(makeSlideshowauto, auto_time);
-makeSlideshow(slideIndex);
+setInterval(makeSlideshowauto, AUTO_TIME);
+makeSlideshow(0);
